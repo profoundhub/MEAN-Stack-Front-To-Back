98842c6fa322ac1fc65dbdb806c7c74059067377
Daniel <github@profoundideation.com>
2018-04-07 13:16:32 -0400

    on Connection OR Error

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const config = require('./config/database');
 // Connect To Database
 mongoose.connect(config.database);
 
+// On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database '+ config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: '+ err);
+});
+
 const app = express();
 
 const users = require('./routes/users');
