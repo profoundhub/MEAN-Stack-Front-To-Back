ba3587a96c6533f85eb435b32a0785af45bd63cb
Daniel <github@profoundideation.com>
2018-04-07 12:59:02 -0400

    clean up, good to go

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
-
 const app = express();
 
 // Port Number
 const port = 3000;
 
-
 // Start Server
 app.listen(port, () => {
-    console.log('Server started on port '+port);
-  });
-  
+    console.log('Server started on port ' + port);
+}); 
