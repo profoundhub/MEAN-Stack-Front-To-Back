622b7960f0f0ad4d18553162474d6721575b3184
Daniel <github@profoundideation.com>
2018-04-07 13:03:25 -0400

    + cors

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const app = express();
 // Port Number
 const port = 3000;
 
+// CORS Middleware
+app.use(cors());
+
+// Index Route
+app.get('/', (req, res) => {
+        res.send('Invalid Endpoint');
+    });
+
 // Start Server
 app.listen(port, () => {
-    console.log('Server started on port ' + port);
-}); 
+        console.log('Server started on port ' + port);
+    }); 
