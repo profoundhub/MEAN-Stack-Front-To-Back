5f15d3ea8a388c9c523f7f47332d09ee5eebf954
Daniel <github@profoundideation.com>
2018-04-07 13:23:57 -0400

    - validate

--- routes/users.js
+++ routes/users.js
@@ -16,9 +16,5 @@ router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
 });
 
-// Validate
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE');
-});
 
 module.exports = router;
