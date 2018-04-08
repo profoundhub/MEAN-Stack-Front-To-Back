58751e28d342086b3a0431f39242c5cdde20b975
Daniel <github@profoundideation.com>
2018-04-08 01:27:59 -0400

    update user.js

--- models/user.js
+++ models/user.js
@@ -20,3 +20,24 @@ const UserSchema = mongoose.Schema({
     required: true
   }
 });
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback) {
+  const query = {username: username}
+  User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if(err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
