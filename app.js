bfc2f34ec3b3f6dc0ae46a7a075d7ff5189964a8
Daniel <github@profoundideation.com>
2018-04-08 02:55:03 -0400

    mess

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ mongoose.connect(config.database);
 
 // On Connection
 mongoose.connection.on('connected', () => {
-    console.log('Connected to database '+ config.database);
+    console.log('Connected to database :' + config.database);
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-    console.log('Database error: '+ err);
+    console.log('Database error: ' + err);
 });
 
 const app = express();
--- routes/users.1.js
+++ routes/users.1.js
@@ -6,27 +6,9 @@ const User = require('../models/user');
 
 // Register
 router.post('/register', (req, res, next) => {  
-    /*
-    
-    let newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password
-    });
 
-    User.addUser(newUser, (err, user) => {
-        if(err) {
-            res.json({success: false, msg:'Failed to Register User'});
-        } else {
-            res.json({success: true, msg:'User Registered'});
-        }
-    });
-    */
-
-   /* 
     res.send('REGISTER');
-   */
+
 });
 
 // Authenticate
