cf8a97df0b8ca31918eb2bceb7b8d76ae04e4255
Daniel <github@profoundideation.com>
2018-04-08 01:29:44 -0400

    up users

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
 // Register
 router.post('/register', (req, res, next) => {
     res.send('REGISTER');
