0f86744f2226e782ad835cad700694fd5e669295
Daniel <github@profoundideation.com>
2018-04-08 03:14:50 -0400

    mess 3

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ mongoose.connect(config.database);
 
 // On Connection
 mongoose.connection.on('connected', () => {
-    console.log('Connected to database :' + config.database);
+  console.log('Connected to database = '+config.database);
 });
 
 // On Error
 mongoose.connection.on('error', (err) => {
-    console.log('Database error: ' + err);
+  console.log('Database error: '+err);
 });
 
 const app = express();
@@ -39,10 +39,10 @@ app.use('/users', users);
 
 // Index Route
 app.get('/', (req, res) => {
-    res.send('Invalid Endpoint');
+  res.send('Invalid Endpoint');
 });
 
 // Start Server
 app.listen(port, () => {
-    console.log('Server started on port ' + port);
-}); 
+  console.log('Server started on port = '+port);
+});
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
 
 // Register
-router.post('/register', (req, res, next) => {  
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+ res.send('Register');
 });
 
 // Authenticate
 router.post('/authenticate', (req, res, next) => {
-    res.send('AUTHENTICATE');
+  res.send('AUTHENTICATE');
 });
 
 // Profile
 router.get('/profile', (req, res, next) => {
-    res.send('PROFILE');
+  res.send('PROFILE');
 });
 
 module.exports = router;
